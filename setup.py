fde43b2b3da4b7a874ec5152adc8b4bd6a0437f6
hetulmehta <hetulmehta08@gmail.com>
2023-05-12 16:30:59 +0530

    setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description="A SMOTE variant called sematic-cosine SMOTE that generates textual synthetic data ",
     long_description=description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/hetulmehta/SC-SMOTE.git",
     license='MIT',
     python_requires='>=3.8',
     install_requires=[]
