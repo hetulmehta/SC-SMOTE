aa780e9b098a764b744569a408854c755c5a84e6
Hetul Mehta <hetulmehta08@gmail.com>
2024-10-14 11:33:04 -0700

    updated package number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
   
 setuptools.setup(
     name="scsmote-package",
-    version="0.0.1",
+    version="0.0.2",
     author="Hetul Mehta",
     author_email="hetulmehta08@gmail.com",
     packages=["scsmote_package"],
